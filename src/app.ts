e841c8cbd99e66007a59301ec8ed63e2a5356b52
D Samal <samal@smarterdharma.com>
2022-10-19 23:44:07 +0530

    changes

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app:Application = express()
 const port:number =  3000
 
 app.get('/user', (req: Request, res: Response) => {
-    let result = {id:1,name:'user1'}
+    let result = {id:2,name:'user2'}
     res.status(200).json(result)
 })
 
